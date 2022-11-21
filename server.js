e3118cd1d7ddc9939a724b235c43d374f480de24
Mohammad Amin Lotfi <a.lotfi1201@gmail.com>
2022-11-21 23:59:18 +0330

    feat: SPA

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname, "src", "static")));
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "src", "index.html"));
+});
+
+const port = process.env.PORT || '3001';
+const server = http.createServer(app);
+server.listen(port);
+
+// app.listen(5620, () => console.log("Server running..."));
